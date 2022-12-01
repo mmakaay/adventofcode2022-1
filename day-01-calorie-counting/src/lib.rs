52bb345a88c5625dc3ff37e680d292f7b91645b8
Miquel van Smoorenburg <mike@langeraar.net>
2022-12-01 16:10:40 +0100

    remove array reversing

--- day-01-calorie-counting/src/lib.rs
+++ day-01-calorie-counting/src/lib.rs
@@ -1,17 +1,17 @@
 fn calorie_list(input: &str) -> Vec<u64> {
     let mut cals = input.split("\n\n")
-        .map(|grp| grp.trim().split("\n").map(|i| i.parse::<u64>().unwrap()).sum::<u64>())
+        .map(|grp| grp.split("\n").filter_map(|i| i.parse::<u64>().ok()).sum::<u64>())
         .collect::<Vec<_>>();
     cals.sort();
-    cals.reverse();
     cals
 }
 
 pub fn part1(input: &str) {
-    println!("highest: {}", calorie_list(input)[0]);
+    let cals = calorie_list(input);
+    println!("highest: {}", cals.iter().rev().next().unwrap());
 }
 
 pub fn part2(input: &str) {
     let cals = calorie_list(input);
-    println!("sum of 3 higest: {}", cals[0] + cals[1] + cals[2]);
+    println!("sum of 3 higest: {}", cals.iter().rev().take(3).sum::<u64>());
 }
