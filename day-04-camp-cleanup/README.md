555b0aaece998aa7c85be9fe4426a16871ae9347
Miquel van Smoorenburg <mike@langeraar.net>
2022-12-04 11:04:09 +0100

    optimized version

--- day-04-camp-cleanup/README.md
+++ day-04-camp-cleanup/README.md
@@ -13,6 +13,8 @@ probably not scale for part 2, but we'll see.
 Runtime: 233.928µs. After setting `RUSTFLAGS='-C target-cpu=native'`,
 220.074µs. It helps, but not a lot.
 
+Runtime after optimizations: 59.07µs.
+
 ### Part 2.
 
 Well that was easy. It's the slightly simpler version of part1. Just
@@ -20,7 +22,23 @@ checking for any overlap, not if one range is completely contained
 by the other.
 
 Runtime: 213.215µs.
+Runtime after optimizations: 52.811µs.
 
-### Optimization.
+### Optimizations.
 
-TBD
+1. We don't have to loop over the ranges on each line, it's always two ranges.
+   So just use `split_once(range, ',').unwrap()`.
+2. Setting a range of bits in a loop is slow, much faster is
+   `(1u128 << (to + 1)) - (1u128 << from)`.
+3. Both part1 and part2 are basically doing the same work, so combining
+   them into one function should halve the total time, and it does.
+   Total runtime for both solutions: 52.36µs.
+4. Splitting and parsing strings could probably be made faster. Add a
+   simple "parse next number and advance index until next non-numeric char"
+   and use that to step over "1-", "2," "3-", "4".
+   Total runtime for both solutions now down to: 28.453µs.
+
+Interesting to note, with 1000 lines of input the difference between
+`input.lines()` and `input.trim().split("\n")` is about 28µs.
+That is a lot!
+
--- day-04-camp-cleanup/src/lib.rs
+++ day-04-camp-cleanup/src/lib.rs
@@ -1,10 +1,8 @@
 
-fn parse_range(range: &str, set: &mut u128) {
+fn parse_range1(range: &str) -> u128 {
     let (l, r) = range.split_once('-').unwrap();
     let (l, r) = (l.parse::<u8>().unwrap(), r.parse::<u8>().unwrap());
-    for i in l ..=r {
-        *set |= 1 << i;
-    }
+    (1u128 << (r + 1)) - (1u128 << l)
 }
 
 pub fn part1(input: &str) {
@@ -12,31 +10,55 @@ pub fn part1(input: &str) {
         .trim()
         .split("\n")
         .map(|line| {
-            let mut ranges = [0u128; 2];
-            line
-                .split(",")
-                .enumerate()
-                .for_each(|(i, r)| parse_range(r, &mut ranges[i]));
-            let and = ranges[0] & ranges[1];
-            (ranges[0] == and || ranges[1] == and) as u32
+            let (l, r) = line.split_once(',').unwrap();
+            let l = parse_range1(l);
+            let r = parse_range1(r);
+            ((l & r) == l || (l & r) == r) as u32
         })
         .sum::<u32>();
     println!("part1: {}", r);
 }
-
 
 pub fn part2(input: &str) {
     let r = input
         .trim()
         .split("\n")
         .map(|line| {
-            let mut ranges = [0u128; 2];
-            line
-                .split(",")
-                .enumerate()
-                .for_each(|(i, r)| parse_range(r, &mut ranges[i]));
-            ((ranges[0] & ranges[1]) != 0) as u32
+            let (l, r) = line.split_once(',').unwrap();
+            let l = parse_range1(l);
+            let r = parse_range1(r);
+            ((l & r) > 0) as u32
         })
         .sum::<u32>();
     println!("part2: {}", r);
+}
+
+fn next_num(r: &[u8], idx: &mut usize) -> u8 {
+    let mut n = 0;
+    while *idx < r.len() && r[*idx] >= b'0' && r[*idx] <= b'9' {
+        n = n * 10 + r[*idx] - b'0';
+        *idx += 1;
+    }
+    *idx += 1;
+    n
+}
+
+fn parse_range(range: &[u8], idx: &mut usize) -> u128 {
+    let l = next_num(range, idx);
+    let r = next_num(range, idx);
+    (1u128 << (r + 1)) - (1u128 << l)
+}
+
+pub fn part1_2(input: &str) {
+    let mut p1 = 0u32;
+    let mut p2 = 0u32;
+    for line in input.trim().as_bytes().split(|b| *b == b'\n') {
+        let mut idx = 0;
+        let l = parse_range(line, &mut idx);
+        let r = parse_range(line, &mut idx);
+        p1 += ((l & r) == l || (l & r) == r) as u32;
+        p2 += ((l & r) > 0) as u32;
+    }
+    println!("part1: {}", p1);
+    println!("part2: {}", p2);
 }
