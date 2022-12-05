b21b87c4392f21f209e41793c26fb8bb811da54c
Miquel van Smoorenburg <mike@langeraar.net>
2022-12-05 12:33:20 +0100

    optimize. 94.657µs.

--- day-05-supply-stacks/src/lib.rs
+++ day-05-supply-stacks/src/lib.rs
@@ -1,10 +1,63 @@
-#[macro_use] extern crate scan_fmt;
-use std::time::Instant;
+
+pub fn part1_2(input: &str) {
+    let mut lines = input.trim_end().split("\n");
+    let mut stacks = Stacks::parse_stacks(&mut lines);
+    let cmds = Cmd::parse_cmds(&mut lines);
+
+    let mut sclone = stacks.clone();
+    sclone.cratemover_9000(&cmds);
+    println!("day-05: part1: {}", sclone.top_crates());
+
+    stacks.cratemover_9001(&cmds);
+    println!("day-05: part2: {}", stacks.top_crates());
+}
+
+struct Cmd {
+    num:    usize,
+    from:   usize,
+    to:     usize,
+}
+
+impl Cmd {
+    fn parse_cmds<'a>(lines: impl Iterator<Item = &'a str>) -> Vec<Cmd> {
+        let mut cmds = Vec::new();
+        for cmd in lines {
+            let mut nums = cmd.split(' ').skip(1).step_by(2).map(|w| atou(w));
+            let [num, from, to] = std::array::from_fn(|_| nums.next().unwrap());
+            cmds.push(Cmd { num, from, to });
+        }
+        cmds
+    }
+}
 
 #[derive(Clone)]
 struct Stacks(Vec<Vec<u8>>);
 
 impl Stacks {
+    fn cratemover_9000<'a>(&mut self, cmds: &[Cmd]) {
+        for cmd in cmds {
+            for _ in 0 .. cmd.num {
+                let c = self.0[cmd.from - 1].pop().unwrap();
+                self.0[cmd.to - 1].push(c);
+            }
+        }
+    }
+
+    fn cratemover_9001<'a>(&mut self, cmds: &[Cmd]) {
+        for cmd in cmds {
+            let mut tmp = std::mem::replace(&mut self.0[cmd.to - 1], Vec::new());
+            let i = self.0[cmd.from-1].len() - cmd.num;
+            let c = &self.0[cmd.from-1][i..];
+            tmp.extend_from_slice(c);
+            self.0[cmd.from-1].truncate(i);
+            self.0[cmd.to - 1] = tmp;
+        }
+    }
+
+    fn top_crates(&self) -> String {
+        self.0.iter().map(|s| s[s.len()-1] as char).collect()
+    }
+
     fn parse_stacks<'a>(mut lines: impl Iterator<Item = &'a str>) -> Stacks {
         let mut stacks = Vec::new();
 
@@ -29,61 +82,13 @@ impl Stacks {
         stacks.iter_mut().for_each(|s| s.reverse());
         Stacks(stacks)
     }
-
-    // Prints the stacks flipped 90 degrees clockwise.
-    #[allow(dead_code)]
-    fn debug(&self) {
-        println!("=== stacks ===");
-        for s in &self.0 {
-            print!("[");
-            for c in s {
-                print!("{}", *c as char);
-            }
-            println!("]\n");
-        }
-    }
-
-    fn cratemover_9000<'a>(&mut self, lines: impl Iterator<Item = &'a str>) {
-        type U = usize;
-        for cmd in lines {
-            let (num, from, to) = scan_fmt!(cmd, "move {} from {} to {}", U, U, U).unwrap();
-            for _ in 0 .. num {
-                let c = self.0[from - 1].pop().unwrap();
-                self.0[to - 1].push(c);
-            }
-        }
-    }
-
-    fn cratemover_9001<'a>(&mut self, lines: impl Iterator<Item = &'a str>) {
-        type U = usize;
-        for cmd in lines {
-            let (num, from, to) = scan_fmt!(cmd, "move {} from {} to {}", U, U, U).unwrap();
-            let i = self.0[from-1].len() - num;
-            let c = self.0[from-1].split_off(i);
-            self.0[to - 1].extend_from_slice(&c);
-        }
-    }
-
-    fn top_crates(&self) -> String {
-        self.0.iter().map(|s| s[s.len()-1] as char).collect()
-    }
 }
 
-pub fn part1_2(input: &str) {
-    let now = Instant::now();
-    let mut lines = input.trim_end().split("\n");
-    let mut stacks = Stacks::parse_stacks(&mut lines);
-    println!("day-05: parsing: {:?}", now.elapsed());
-
-    let now = Instant::now();
-    let mut sclone = stacks.clone();
-    let mut lclone = lines.clone();
-    sclone.cratemover_9000(&mut lclone);
-    println!("day-05: part1: {}", sclone.top_crates());
-    println!("day-05: part1: {:?}", now.elapsed());
-
-    let now = Instant::now();
-    stacks.cratemover_9001(&mut lines);
-    println!("day-05: part2: {}", stacks.top_crates());
-    println!("day-05: part2: {:?}", now.elapsed());
+fn atou(r: &str) -> usize {
+    let mut n = 0;
+    let r = r.as_bytes();
+    for i in 0 .. r.len() {
+        n = n * 10 + (r[i] - b'0') as usize;
+    }
+    n
 }
